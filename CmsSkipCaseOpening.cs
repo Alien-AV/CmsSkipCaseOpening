5e889aa1ea26a72b9aedf8ecee4a864af71037da
Alien_AV <alienav@gmail.com>
2019-06-05 22:02:10 +0300

    initial commit

---
+++ CmsSkipCaseOpening.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Harmony12;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace CmsSkipCaseOpening
+{
+    public static class CmsSkipCaseOpening
+    {
+        public static UnityModManager.ModEntry ModEntry;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            ModEntry = modEntry;
+
+            HarmonyInstance.Create(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(CaseOpening))]
+    [HarmonyPatch("Prepare")]
+    public class CaseOpeningPreparePatcher
+    {
+        [HarmonyPrefix]
+        public static void Prefix(CaseOpening __instance)
+        {
+            __instance.GetComponent<Animator>().speed = 10;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# CmsSkipCaseOpening
+Car Mechanic Simulator 2018 Mod
+
+# Installation
+1. Install Unity Mod Manager https://www.nexusmods.com/site/mods/21
+2. Download the CmsSkipCaseOpening.zip file from "Releases" in GitHub
+3. Activate the mod in Unity Mod Manager
+
+# Usage
+The case opening animations is sped up 10x.
